7bec6866d21d425d85c2f9a26ede4ad22edd8eb0
NatPuff <cesarp102104@gmail.com>
2021-11-08 10:39:14 -0600

    progress
    few buttons and stuff working

--- UIKITBullseye/ViewController.swift
+++ UIKITBullseye/ViewController.swift
@@ -8,25 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue = 50
+    var targetValue = 0
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let rVal = slider.value.rounded()
+        currentValue = Int(rVal)
+        startNewRound()
     }
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Sussy baka", message: "I know you took my fortnite card", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Joe mama", style: .default, handler: nil)
+        
+        let msg = "The value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let alert = UIAlertController(title: "Sussy Baka", message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
-        
+        startNewRound()
         
         
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
-        
+        let rVal = slider.value.rounded()
+        currentValue = Int(rVal)
     }
-
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
 
